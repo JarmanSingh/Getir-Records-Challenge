54926cf5d4c32d421eba605ad75c86708be7269d
“Jarman <jarman.singh@rapido.bike>
2022-01-02 19:08:37 +0530

    added prod config values

---
+++ config/production.js
@@ -0,0 +1,5 @@
+module.exports = {
+    env: 'production',
+    timeout: 5000,
+    mongo_url: "mongodb+srv://challengeUser:WUMglwNBaydH8Yvu@challenge-xzwqd.mongodb.net/getir-case-study?retryWrites=true",
+};
